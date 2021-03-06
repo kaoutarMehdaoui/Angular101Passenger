d47c0066526fb77f6268a23148c2fd5cc3b1bb4f
kaoutarMehdaoui <79928656+kaoutarMehdaoui@users.noreply.github.com>
2021-03-06 17:42:49 +0100

    HTTp

--- src/app/passenger/containers/passenger.service.ts
+++ src/app/passenger/containers/passenger.service.ts
@@ -1,12 +1,22 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
 
 import { Passenger, passengers } from "src/assets/passengers";
+const url='http://localhost:3000/passengers';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({ providedIn: "root" })
 export class PassengerService {
-  constructor(private htpp: HttpClient) {}
+  constructor(private http: HttpClient) {}
   public getPassengers(): Observable<Passenger[]> {
-    return of(passengers);
+    return this.http.get<Passenger[]>(url);
   }
+  public editPassenger(passenger:Passenger):Observable<any>{
+    
+    return this.http.put(`${url}/${passenger.id}`,httpOptions)
+}
+
 }
